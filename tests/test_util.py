65e0663f1d124188d599f16fd2c3816807872645
Macklan Weinstein <macklan.weinstein@gmail.com>
2023-11-10 09:25:37 -0800

    :rotating_light: Fix type in test function

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,12 +1,14 @@
 """Tests for utility functions."""
 
 
+from pathlib import Path
+
 from auralmatics import util
 
 
 def test_read_wave_range() -> None:
     """Samples read from wave file are between -1 and 1."""
-    _, samples = util.read_wave("data/school_bell.wav")
+    _, samples = util.read_wave(Path("data/school_bell.wav"))
 
     assert samples.max() <= 1
     assert samples.min() >= -1
